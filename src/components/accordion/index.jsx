1862630da3cd1a49699569f01f3fb89245d7ad4a
nikita-sarkar <nikita2019sarkar@gmail.com>
2025-06-11 23:38:31 +0530

    Fetching data in accordion component

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -1,5 +1,24 @@
+import data from "./data";
+import React, { useState } from 'react';
+
 export default function Accordion() {
-    return <div>
-        Accordion
-    </div>;
+
+    const [selected, setSelected] = useState(null);
+
+    return (
+        <div className="wrapper">
+            <div className="accordion">
+                {
+                        data && data.length > 0 ?
+                        (data.map((dataItem) =>
+                        (<div className="item">
+                            <div className="title">
+                                <h3>{dataItem.question}</h3>
+                                <span>+</span>
+                            </div>
+                        </div>)))
+                        : (<div>No data found!</div>)}
+            </div>
+        </div>
+    )
 }
