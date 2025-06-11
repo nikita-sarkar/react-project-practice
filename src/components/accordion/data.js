6f2b0c72b42e7b68daf4b2dc361a47d929168c12
nikita-sarkar <nikita2019sarkar@gmail.com>
2025-06-11 21:57:01 +0530

    Creating accordion component

---
+++ src/components/accordion/data.js
@@ -0,0 +1,19 @@
+const data =[
+{
+    id : '1',
+    question: "What is the weather like today?",
+    answer: "It is sunny"
+},
+{
+    id : '2',
+    question: "What is your cat's name?",
+    answer: "Meow"
+},
+{
+    id : '3',
+    question: "What fruit do you like?",
+    answer: "Apples"
+}
+];
+
+export default data;
