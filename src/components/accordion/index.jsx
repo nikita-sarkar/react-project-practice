f79c7317ec34697dd3fb166f48164309683b86e8
nikita-sarkar <nikita2019sarkar@gmail.com>
2025-06-11 23:56:38 +0530

    Showing answer upon span click

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -5,6 +5,12 @@ export default function Accordion() {
 
     const [selected, setSelected] = useState(null);
 
+    function handleSingleSelection(getCurrentId)
+    {
+        setSelected(getCurrentId);
+    }
+
+
     return (
         <div className="wrapper">
             <div className="accordion">
@@ -12,9 +18,15 @@ export default function Accordion() {
                         data && data.length > 0 ?
                         (data.map((dataItem) =>
                         (<div className="item">
-                            <div className="title">
+                            <div onClick={()=>handleSingleSelection(dataItem.id)} className="title">
                                 <h3>{dataItem.question}</h3>
                                 <span>+</span>
+                                <div>{
+                                    selected === dataItem.id ?
+                                (
+                                <div className="content">{dataItem.answer}</div>
+                                )
+                                : null}</div>
                             </div>
                         </div>)))
                         : (<div>No data found!</div>)}
